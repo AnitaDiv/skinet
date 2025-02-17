a8b0a5e893d3a7dcf2294debfa16938eedf07b0e
AnitaDiv <anitaostojic@yahoo.com>
2025-02-17 20:17:42 +0100

    End of section 18

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d43bf954510912edbb837f9a1717f391a55b5af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc276d385d21c0144c17f18b410ac00b3046878d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51QqceEEDsvKVSSR33PocDJ9Jk9oKwdhWQG3NtvZou3JDa2P4aWyV3YiqyyOIUxUCmtqSmmKHvImRnUNRTn1igsl900Hbyvveh4'
 };
